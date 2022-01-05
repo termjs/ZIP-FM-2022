383a55870e26c7c4acc061bf7c76b3ffdffc45fb
Term <74162245+termissues@users.noreply.github.com>
2022-01-05 04:53:02 -0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const client = require("../index");
 
 client.on("ready", async (client) => {
     console.log(`${client.user.tag} is Ready to Roll!`)
-    client.user.setPresence({ activities: [{ name: `/pagalba`, type: 'LISTENING' }], status: 'online' });
+    client.user.setPresence({ activities: [{ name: `/pagalba`, type: 'PLAYING' }], status: 'online' });
 });
